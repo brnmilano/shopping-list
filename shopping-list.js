194ec6abee5288635bf4fe3470055a4544afcb50
Bruno <brn.milano@hotmail.com>
2024-11-09 19:30:55 -0300

    Continuação da lógica para adicionar novos itens

--- shopping-list.js
+++ shopping-list.js
@@ -1,26 +1,80 @@
+// Captura o nó do formulário
 const form = document.querySelector("form");
-const input = document.querySelector("#input-add-item");
-
+// Captura o valor input
+const input = document.querySelector("#input-add-item").value;
+// Captura o nó da ul
+const ulContainer = document.querySelector("#ul-container");
+// Captura a span com o nome do item na lista
 const item = document.querySelector("#item-text");
-const listItems = document.querySelector("#list-items");
+// Captura o botão para deletar um item da lista
+const deleteItem = document.querySelector(".delete-button");
 
-const deleteItem = document.querySelector("#delete-item");
+// Cria um toast que é disparado caso o usuário tente adicionar um item sem value
+function toastMessage() {
+  Toastify({
+    text: "Essa é uma notificação Toastify!",
+    duration: 3000,
+    gravity: "bottom", // Posição: "top" ou "bottom"
+    position: "center", // Posição: "left", "center" ou "right"
+    style: {
+      background: "#ff0000", // Cores de fundo
+    },
+  }).showToast();
+}
 
 let itemsArray = [];
 
+// Adiciona um item dentro do array itemsArray
 const addItem = (item) => {
   itemsArray.unshift(item);
+};
 
-  console.log(itemsArray);
+// Remove um item do array itemsArray
+const removeItem = (item) => {
+  itemsArray.shift(item);
+};
+
+// Cria um novo li na DOM com o elemento que foi criado
+const createNewElements = () => {
+  const li = document.createElement("li");
+  li.classList.add("list-items");
+
+  itemsArray.map((item) => {
+    li.innerHTML = `
+      <div class="item">
+        <input type="checkbox" />
+
+        <span id="item-text">${item}</span>
+      </div>
+
+      <button class="delete-button">
+        <img src="./img/trashicon.svg" alt="" />
+      </button>
+    `;
+
+    ulContainer.append(li);
+  });
+
+  // const deleteButton = li.querySelector(".delete-button");
+  // const itemInSpan = li.querySelector("#item-text").innerText;
+
+  // console.log(deleteButton);
+
+  // deleteButton.addEventListener("click", () => {
+  //   removeItem(itemInSpan);
+  // });
 };
 
 form.onsubmit = async (event) => {
   event.preventDefault();
 
-  const inputValue = input.value;
+  if (input.length === 0) {
+    toastMessage();
 
-  console.log(inputValue);
-  console.log(typeof inputValue);
+    return;
+  }
 
   addItem(inputValue);
+
+  createNewElements();
 };
